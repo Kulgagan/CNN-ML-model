fixed weights gradient in dense layer

ML-Model/activation_layer.py
@@ -1,6 +1,5 @@
 import numpy as np
 from base_layer import Layer
-#comment later 
 
 class Activation(Layer):
     def __init__(self, activation, activation_prime):   
@@ -13,4 +12,7 @@ class Activation(Layer):
     
     def backward(self, output_gradient, learing_rate):
         return np.multiply(output_gradient, self.activation_prime(self.input))
+    
+
+#comment on this
     

ML-Model/dense.py
@@ -1,17 +1,18 @@
 import numpy as np
-import base_layer as Layer
+from layer import Layer
 
 class Dense(Layer):
-    def __init__ (self, input_size, output_size):   #initializing input and output neurons
-        self.weights = np.random.randn(output_size, input_size) #initializing bias and weights as random numbers (will change in the future)
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size, input_size)
         self.bias = np.random.randn(output_size, 1)
-        
+
     def forward(self, input):
         self.input = input
         return np.dot(self.weights, self.input) + self.bias
-    
-    def backward(self, output_gradient, learning_rate): #calculates the derivative of the error wrt the weights
-        weights_gradient = np.dot(output_gradient, learning_rate)   #derivative of error wrt to the basis
-        self.weights -= learning_rate * weights_gradient    #updating the paramenters with gradient decent
+
+    def backward(self, output_gradient, learning_rate):
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        input_gradient = np.dot(self.weights.T, output_gradient)
+        self.weights -= learning_rate * weights_gradient
         self.bias -= learning_rate * output_gradient
-        return np.dot(self.weights.T, output_gradient)  #return the derivative of the error wrt to the input(for other layers)
+        return input_gradient