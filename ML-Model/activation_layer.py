f6c3be2fd53b2432303002a493e93ec6bcfaa8b2
Kulgagan <Kulgagan08@gmail.com>
2025-01-04 20:37:25 -0700

    activation layer done

---
+++ ML-Model/activation_layer.py
@@ -0,0 +1,16 @@
+import numpy as np
+from base_layer import Layer
+#comment later 
+
+class Activation(Layer):
+    def __init__(self, activation, activation_prime):   
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward(self, input):
+        self.input = input
+        return self.activation(self.input)
+    
+    def backward(self, output_gradient, learing_rate):
+        return np.multiply(output_gradient, self.activation_prime(self.input))
+    
