a7b744af9e7cf53df6483297568b0e447d543fb2
Kulgagan <Kulgagan08@gmail.com>
2025-02-03 18:37:51 -0700

    dense layer explanation

--- ML-Model/dense_layer.py
+++ ML-Model/dense_layer.py
@@ -1,7 +1,19 @@
 import numpy as np
 from base_layer import Layer
 """
-Explanation...
+Fully connected dense layer (every neuron in the previous layer is connected to every neuron in current layer)
+1. initialization: Weights and biases are initialized randomly so each neuron can learn different things
+2. forward propagation: 
+- computes Y = W (dot) X + b
+- weights determine the stregth of each connection
+- bias allows network to shigt the output space so it can model more complex functions
+- activation func should be applied after this
+3. Uses back propogation (method of updationg weights and biases based on gradient descent). (overall calculates how changing weights would affect the function) 
+    1. compute the gradient of the loss with respect to weights
+    2. compute the gradient of the loss with respect to the bias
+    3. compute the gradient of the loss with respect to the input to pass it to the previous layer
+4. update parameters: Adjust the wieghts and biases using gradient descent
+(goal is to minimize loss function. as it gets smaller, the model gets better at training. sort of its metric of accuracy) 
 """
 
 class Dense(Layer):
@@ -15,7 +27,7 @@ class Dense(Layer):
 
     def backward(self, output_gradient, learning_rate): #calculates the derivative of the error wrt the weights
         weights_gradient = np.dot(output_gradient, self.input.T)   #derivative of error wrt to the basis
-        input_gradient = np.dot(self.weights.T, output_gradient)    #the derivative of the error wrt to the input(for other)
+        input_gradient = np.dot(self.weights.T, output_gradient)    #the derivative of the error wrt to the input(for other) (returned)
         self.weights -= learning_rate * weights_gradient    #updating the paramenters with gradient decent
         self.bias -= learning_rate * output_gradient
         return input_gradient   #returns the derivative of the error wrt the input
